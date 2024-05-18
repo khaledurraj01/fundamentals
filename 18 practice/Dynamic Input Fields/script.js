52d61ca430876feaee5ea3c6882bc42eb2f0d93a
khaledur <khaledur.rahman.raj@gmail.com>
2024-05-18 20:27:43 +0600

    new practice

---
+++ 18 practice/Dynamic Input Fields/script.js
@@ -0,0 +1,31 @@
+const addBtn = document.querySelector(".add");
+const input = document.querySelector(".inp-group");
+
+function removeInput(){
+    this.parentElement.remove();
+}
+
+function addInput(){
+    const name = document.createElement("input");
+    name.type="text";
+    name.placeholder = "Enter Your Name";
+    
+    const email = document.createElement("input");
+    email.type="email";
+    email.placeholder = "Enter Your Email"
+
+    const btn =document.createElement("a");
+    btn.className = "delete";
+    btn.innerHTML = "&times";
+
+    btn.addEventListener("click", removeInput);
+
+    const flex=document.createElement("div");
+    flex.className = "flex";
+
+    input.appendChild(flex);
+    flex.appendChild(name);
+    flex.appendChild(email);
+    flex.appendChild(btn);
+}
+addBtn.addEventListener("click",addInput);
